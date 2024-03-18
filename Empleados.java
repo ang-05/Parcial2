82e5260c666b3883f3709f123c0fc7e8bab5b377
AngelaRogel 05 <arogel8@alu.ucam.edu>
2024-03-18 10:20:28 +0100

    Creación clase Empleados con los métodos del main mejorados y sustituidos

---
+++ Empleados.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Empleados {
+    private static ArrayList<Empleado> empleados_lista;
+
+    public Empleados() {
+        empleados_lista = new ArrayList<>();
+    }
+
+    public void listadoEmpleados() {
+        for (Empleado empleado : empleados_lista) {
+            System.out.println(empleado.toString());
+        }
+    }
+
+    public void aumentarSalario(double porcentaje) {
+        for (Empleado empleado : empleados_lista) {
+            double nuevoSalario = empleado.getSalario() * (1 + porcentaje / 100);
+            empleado.setSalario(nuevoSalario);
+        }
+    }
+
+    public static void addEmpleado(Empleado empleado){
+        empleados_lista.add(empleado);
+    }
+
+    public static void darAlta(){
+        Empleado e1 = new Empleado("Juan", "Desarrollador", 50000);
+        Empleado e2 = new Empleado("María", "Diseñadora", 45000);
+        Empleado e3 = new Empleado("Pedro", "Gerente", 60000);
+        addEmpleado(e1);
+        addEmpleado(e2);
+        addEmpleado(e3);
+
+    }
+
+
+}
--- SistemaGestionEmpleados.java
+++ SistemaGestionEmpleados.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SistemaGestionEmpleados {
@@ -16,24 +17,15 @@ public class SistemaGestionEmpleados {
     }
 
     public static void main(String[] args) {
-        Empleado[] empleados = new Empleado[3];
-        empleados[0] = new Empleado("Juan", "Desarrollador", 50000);
-        empleados[1] = new Empleado("María", "Diseñadora", 45000);
-        empleados[2] = new Empleado("Pedro", "Gerente", 60000);
-
-        SistemaGestionEmpleados sistema = new SistemaGestionEmpleados(empleados);
+       Empleados empleados = new Empleados();
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Introduzca el porcentaje de aumento de salario: ");
         double porcentaje = scanner.nextDouble();
-
-        sistema.aumentarSalario(porcentaje);
+        empleados.aumentarSalario(porcentaje);
         
-        System.out.println("Lista de Empleados:");
-        for (Empleado empleado : empleados) {
-            System.out.println(empleado);
-        }
-
+        empleados.listadoEmpleados();
+        
         scanner.close();
     }
 }
